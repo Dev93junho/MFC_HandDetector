789463617edba0d8929db751e7143992140f1a65
JunhoShin_personal <jhshin9345@gmail.com>
2022-01-21 11:48:06 +0900

    updated mfc_webcam

---
+++ HandDetector/HandDetector/HandDetectorDlg.h
@@ -0,0 +1,45 @@
+
+// HandDetectorDlg.h : 헤더 파일
+//
+
+#pragma once
+#include "afxwin.h"
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+
+// CHandDetectorDlg 대화 상자
+class CHandDetectorDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CHandDetectorDlg(CWnd* pParent = NULL);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_HANDDETECTOR_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	CStatic m_picture;
+	afx_msg void OnDestroy();
+	afx_msg void OnTimer(UINT_PTR nIDEvent);
+
+	VideoCapture* capture;
+	Mat mat_frame;
+	CImage cimage_mfc;
+
+};
