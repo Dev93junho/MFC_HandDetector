269f6062869aeb71337c60afbac3d514d9668aa4
JunhoShin_personal <jhshin9345@gmail.com>
2022-02-04 16:12:32 +0900

    init cnn

---
+++ HandDetector/cnn.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "tiny-dnn-master/tiny_dnn/activations/activation_layer.h"
+#include "tiny-dnn-master/tiny_dnn/lossfunctions/loss_function.h"
+#include "tiny-dnn-master/tiny_dnn/layers/convolutional_layer.h"
+#include "tiny-dnn-master/tiny_dnn/core/kernels/conv2d_op.h"
+
+void cnn(__DATAENTRY__) {
+	
+}
