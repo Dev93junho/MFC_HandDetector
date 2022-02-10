8098ba20b0305a19d1bbdfcf97a8deba1e82c496
JunhoShin_personal <jhshin9345@gmail.com>
2022-02-10 17:05:48 +0900

    updated opencv_test

--- HandDetector/opencv_test.py
+++ HandDetector/opencv_test.py
@@ -1,55 +1,38 @@
 from __future__ import print_function
-import cv2 as cv
-import argparse
-def detectAndDisplay(frame):
-    frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    frame_gray = cv.equalizeHist(frame_gray)
-    #-- Detect faces
-    faces = face_cascade.detectMultiScale(frame_gray)
-    for (x,y,w,h) in faces:
-        center = (x + w//2, y + h//2)
-        frame = cv.ellipse(frame, center, (w//2, h//2), 0, 0, 360, (255, 0, 255), 4)
-        faceROI = frame_gray[y:y+h,x:x+w]
-        #-- In each face, detect eyes
-        eyes = eyes_cascade.detectMultiScale(faceROI)
-        for (x2,y2,w2,h2) in eyes:
-            eye_center = (x + x2 + w2//2, y + y2 + h2//2)
-            radius = int(round((w2 + h2)*0.25))
-            frame = cv.circle(frame, eye_center, radius, (255, 0, 0 ), 4)
-    cv.imshow('Capture - Face detection', frame)
-parser = argparse.ArgumentParser(description='Code for Cascade Classifier tutorial.')
-parser.add_argument('--face_cascade', help='Path to face cascade.', default='data/haarcascades/haarcascade_frontalface_alt.xml')
-parser.add_argument('--eyes_cascade', help='Path to eyes cascade.', default='data/haarcascades/haarcascade_eye_tree_eyeglasses.xml')
-parser.add_argument('--camera', help='Camera devide number.', type=int, default=0)
-args = parser.parse_args()
-face_cascade_name = args.face_cascade
-eyes_cascade_name = args.eyes_cascade
-face_cascade = cv.CascadeClassifier()
-eyes_cascade = cv.CascadeClassifier()
-#-- 1. Load the cascades
-if not face_cascade.load(cv.samples.findFile(face_cascade_name)):
-    print('--(!)Error loading face cascade')
-    exit(0)
-if not eyes_cascade.load(cv.samples.findFile(eyes_cascade_name)):
-    print('--(!)Error loading eyes cascade')
-    exit(0)
-camera_device = args.camera
-#-- 2. Read the video stream
-cap = cv.VideoCapture(camera_device)
-if not cap.isOpened:
-    print('--(!)Error opening video capture')
-    exit(0)
+import cv2 
+
+
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontface.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
 while True:
     ret, frame = cap.read()
-    if frame is None:
-        print('--(!) No captured frame -- Break!')
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+
+
+    faces = face_cascade.detectMultiScale(cap, 1.3, 5)  
+    for (x,y,w,h) in faces:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for (ex,ey,ew,eh) in eyes:
+            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+
+    cv2.imshow("Test", frame)        
+    if cv2.waitKey(1) == ord('q'):
         break
-    detectAndDisplay(frame)
-    if cv.waitKey(10) == 27:
-        break
-    
-    
-    
+
+
+cap.release()
+cv2.destroyAllWindows()
+
+
 import logging
 import logging.config
 import ctypes
