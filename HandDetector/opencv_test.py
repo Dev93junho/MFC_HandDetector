ff558c6fc332a2a6eb8704ff6d5e474d4afa4e34
JunhoShin_personal <jhshin9345@gmail.com>
2022-02-10 17:27:32 +0900

    updated test.py

--- HandDetector/opencv_test.py
+++ HandDetector/opencv_test.py
@@ -15,7 +15,7 @@ while True:
     gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
 
 
-    faces = face_cascade.detectMultiScale(cap, 1.3, 5)  
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)  
     for (x,y,w,h) in faces:
         cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
         roi_gray = gray[y:y+h, x:x+w]
